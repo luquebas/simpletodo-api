c1786d9a2606335a0c11240d095e69fc2936b508
Luquebas <lucas.fonseca.sesisenaisp@gmail.com>
2024-01-15 01:57:29 -0300

    TaskService CRUD

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -4,16 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.example.demo.models.User;
-import com.example.demo.repositories.TaskRepository;
 import com.example.demo.repositories.UserRepository;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private TaskRepository taskRepository;
 
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
@@ -26,7 +22,6 @@ public class UserService {
     public User create(User obj) {
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
@@ -42,7 +37,7 @@ public class UserService {
         try {
             this.userRepository.deleteById(id);
         } catch(Exception e) {
-            throw new RuntimeException("Não é possível excluir, há tarefas relacionadas!");
+            throw new RuntimeException("Não é possível excluir, há entidades relacionadas!");
         }
     }
 
